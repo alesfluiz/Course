9a88055def9adf136f914d2bf00125f561b66853
Alesf Luiz <alesfluiz@gmail.com>
2023-01-27 13:56:18 -0300

    initial commit

---
+++ Course/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Course.Entities.Enums
+{
+    internal enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,21 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+
+            };
+            Console.WriteLine(order);
+        }
+    }
+}
